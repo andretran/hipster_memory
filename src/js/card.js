b98f31a4b85f2582f239fb9cb464258f2c0bb05d
andretran <Andre.n.tran@gmail.com>
2014-09-03 16:05:23 -0700

    implemented card flipping w/ angular

--- src/js/card.js
+++ src/js/card.js
@@ -1,6 +1,10 @@
 var memoryApp = memoryApp || {}
 
 var Card = memoryApp.card = function Card(){
-  this.hidden = true,
+  this.flipped = false,
   this.face = "images/cardback.jpg"
+}
+
+Card.prototype.flipCard = function(){
+  this.flipped = !this.flipped;
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,16 @@
 $(function(){
-  _.each($('.front'),function(card){
-    var $card = $(card);
-    $card.click(function() {
-      $card.flip({
-        direction: 'lr',
-        content: '<img src="images/front.jpg" class="front">'
-      });
-    });
-  });
+  // // var $card = $("<div class='card'><img src='images/front.jpg' class='back side'></div>");
+  // for (var i = 0; i < 12; i++){
+  //   $card = $("<div class='card'></div>");
+  //   $('.container').append($card);
+  //   $card.append($("<img src='images/front.jpg' class='front side'>"));
+  // }
+
+  // var cards = $('.card');
+  // _.each(cards, function(card){
+  //   setTimeout(function(){
+  //     setInterval(function(){
+  //       $(card).css({transform: 'rotateY( -50deg )'});
+  //     }, 2000)}, Math.random() * 1000);
+  // });
 });
