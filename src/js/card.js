ac8ba38877b847b958fbda93f68b299905619974
andretran <Andre.n.tran@gmail.com>
2014-09-03 18:48:53 -0700

    buggy oscilatting cards

--- src/js/card.js
+++ src/js/card.js
@@ -1,10 +1,23 @@
 var memoryApp = memoryApp || {}
 
-var Card = memoryApp.card = function Card(){
+var Card = memoryApp.card = function Card(logo_id){
   this.flipped = false,
-  this.face = "images/cardback.jpg"
+  this.logo = 'images/' + logo_id + '.png',
+  this.back = "images/cardback.jpg",
+  this.position = null;
 }
 
 Card.prototype.flipCard = function(){
   this.flipped = !this.flipped;
+}
+
+Card.prototype.oscillate = function(){
+  if (this.flipped){
+    return;
+  }else if (this.position === 'left'){
+    this.position = 'right';
+  }else{
+    this.position = 'left';
+  }
+  console.log('working');
 }
--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,8 +1,13 @@
 var memoryApp = angular.module('memoryApp', []);
 
-memoryApp.controller('CardsCtrl', function ($scope){
+memoryApp.controller('CardsCtrl', function ($scope, $interval){
   var cards = $scope.cards = [];
+  var logos = _.shuffle([0,0,1,1,2,2,3,3,4,4,5,5]);
   for (var i = 0; i < 12; i++){
-    cards.push(new Card());
+    new_card = new Card(logos[i]);
+    $interval(new_card.oscillate, 2000);
+    cards.push(new_card);
+
   }
+  cards = _.shuffle(cards);
 });
